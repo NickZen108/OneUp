8835572019f60c082d75269c9df7dfd5f9cefcfc
NickZen108 <nicolaipetersen108@gmail.com>
2026-07-18 11:35:17 +0200

    Fix Android debug APK build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'dk.oneup.app',
-  appName: 'OneUp',
-  webDir: 'dist'
-};
-
-export default config;
